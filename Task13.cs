ad313d8dee926d08e24d6b98e149a6cc66b9888f
Platon265 <119070658+Platon265@users.noreply.github.com>
2022-12-06 23:58:34 +0300

    Create Task13.cs

---
+++ Task13.cs
@@ -0,0 +1,23 @@
+Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine()!);
+
+if (number < 0 ) number = -number;
+
+if (number < 100)
+{
+    Console.WriteLine("третьей цифры нет");
+}
+else
+{
+while (number > 999)
+{
+    number = number / 10;
+}
+int result = number % 10;
+
+Console.WriteLine(result);
+}
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
